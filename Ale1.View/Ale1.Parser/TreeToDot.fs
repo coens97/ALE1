2b0d5cd89b31e0462c02fdbcfadb93d15b209e09
Coen <coenstange@me.com>
2017-09-01 12:49:02 +0200

    + add some non working code

---
+++ Ale1.View/Ale1.Parser/TreeToDot.fs
@@ -0,0 +1,40 @@
+﻿module Ale1.Parser.TreeToDot
+
+open Ale1.Common.TreeNode
+open System
+
+// Change enumeration to the according ASCII character
+let private operandToText(operand : OperandValue) : string = 
+    match operand with
+    | OperandValue.Not -> "~"
+    | OperandValue.Implication -> ">"
+    | OperandValue.BiImplication -> "="
+    | OperandValue.And -> "&"
+    | OperandValue.Or -> "|"
+    | _ -> raise (new ArgumentException("Can't recognise enumuration value of OperandValue when parsing tree")) // No match
+
+// Itteration is a recursive function which "walks through" the tree
+// It returns a list of string, because of the assumption that list works best in F# recursion
+let rec private iteration (inputTree : ITreeNode) : string list = 
+    match inputTree with
+    | :? TreeVariable as v -> [ v.Name ] // When node match with a variable only return that
+    | :? TreeOperand as o->
+        match o.NodeValue with // The NOT operand is the only without a right node
+        | OperandValue.Not -> 
+            [operandToText o.NodeValue; "("] @
+            iteration o.Left @
+            [ ")" ]
+        | _ ->
+            [operandToText o.NodeValue; "("] @
+            iteration o.Left @
+            [ "," ] @
+            iteration o.Right @
+            [ ")" ]
+    | _ -> raise (new ArgumentException("Can't recognise ITreeNode type when parsing tree")) // No match
+
+let ToText (inputTree : ITreeNode) : string[] =
+    ["graph logic {" ; "node [ fontname = \"Arial\" ]"] @ // Add static first line of document
+    iteration inputTree @
+    ["}"] // Add last line of document
+    |> List.toArray
+    
