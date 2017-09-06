23c9761db4de7ca1c66f8168bf856f7251ded814
Coen <coenstange@me.com>
2017-09-06 21:37:41 +0200

    + add truthtable dto

---
+++ Ale1.View/Ale1.Common/TruthTable/TruthTable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ale1.Common.TruthTable
+{
+    public class TruthTable
+    {
+        public string[] Headers { get; set; }
+        public string[][] Values { get; set; }
+    }
+}
