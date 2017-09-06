6c58ec1fa0acc890a29a15e386a5f73ff2288bf6
Coen <coenstange@me.com>
2017-09-06 11:14:49 +0200

    + add getting variables

---
+++ Ale1.View/Ale1.Parser.Test/TruthTableTest.cs
@@ -0,0 +1,35 @@
+﻿using Ale1.Functional;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ale1.Parser.Test
+{
+    [TestClass]
+    public class TruthTableTest
+    {
+        [TestMethod]
+        public void TestTreeVariables()
+        {
+            var tests = new Tuple<string, string[]>[] {
+                new Tuple<string, string[]>("&(a,a)", new [] { "a" }),
+                new Tuple<string, string[]>(" &  ( a , a )  ", new [] { "a" }),
+                new Tuple<string, string[]>("~(dank memes)", new [] { "dankmemes" }),
+                new Tuple<string, string[]>("&(Dab,|(F,a))", new [] { "a", "Dab", "F" }),
+                new Tuple<string, string[]>("&(car,|(bike,&(Train, car)))", new [] { "bike", "car", "Train" })
+            };
+
+            foreach (var test in tests)
+            {
+                var tree = TextToTree.Parse(test.Item1);
+                var results = TreeToTruthTable.AllTreeVariables(tree);
+                Assert.IsTrue(Enumerable.SequenceEqual(test.Item2, results), 
+                    $"To tree variable doesn't give expected result, expected [{string.Join(",", test.Item2)}] but got {results.ToString()}");
+                Assert.IsFalse(Enumerable.SequenceEqual(test.Item2, new[] { "This should always fail" }), "Compare lists fails");
+            }
+        }
+    }
+}
---
+++ Ale1.View/Ale1.Parser/TreeToTruthTable.fs
@@ -0,0 +1,23 @@
+﻿module Ale1.Functional.TreeToTruthTable
+
+open Ale1.Common.TreeNode
+open System
+open System.Linq
+
+let rec private AllTreeVariablesItteration(inputTree : ITreeNode) : string list = 
+    match inputTree with
+    | :? TreeVariable as v -> [v.Name] // When node match with a variable only return that
+    | :? TreeOperand as o->
+        match o.NodeValue with // The NOT operand is the only without a right node
+        | OperandValue.Not -> 
+            AllTreeVariablesItteration o.Left
+        | _ ->
+            AllTreeVariablesItteration o.Left @
+            AllTreeVariablesItteration o.Right
+    | _ -> raise (new ArgumentException("Can't recognise ITreeNode type when parsing tree")) // No match
+
+let AllTreeVariables(inputTree : ITreeNode) : string list =
+    inputTree
+    |> AllTreeVariablesItteration
+    |> List.distinct
+    |> List.sortWith (fun a b -> String.Compare(a, b))
