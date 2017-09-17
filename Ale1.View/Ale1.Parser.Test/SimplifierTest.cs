cf4c33a7d5dae4acb6c8463f944a90d310081f24
Coen <coenstange@me.com>
2017-09-17 10:47:45 +0200

    + commiting failed tests for fun and profit

--- Ale1.View/Ale1.Parser.Test/SimplifierTest.cs
+++ Ale1.View/Ale1.Parser.Test/SimplifierTest.cs
@@ -41,5 +41,43 @@ namespace Ale1.Parser.Test
                 Assert.IsTrue(Enumerable.SequenceEqual(row.Item4, result));
             }
         }
+
+        [TestMethod]
+        public void TestArraySliceInsert()
+        {
+            // This tuple almost deserve a class
+            var tests = new Tuple<int, int, FSharpOption<bool>[]>[]{
+                new Tuple<int, int, FSharpOption<bool>[]>(
+                    0,1,
+                    new [] {FSharpOption<bool>.Some(true), FSharpOption<bool>.Some(true)}),
+                new Tuple<int, int, FSharpOption<bool>[]>(
+                    1,2,
+                    new [] { FSharpOption<bool>.Some(true), FSharpOption<bool>.Some(false), FSharpOption<bool>.Some(false), FSharpOption<bool>.Some(true)}),
+                new Tuple<int, int, FSharpOption<bool>[]>(
+                    1,2,
+                    new [] { FSharpOption<bool>.Some(false), FSharpOption<bool>.Some(true), FSharpOption<bool>.Some(false)}),
+                new Tuple<int, int, FSharpOption<bool>[]>(
+                    2,1,
+                    new [] { FSharpOption<bool>.Some(false), FSharpOption<bool>.Some(true), FSharpOption<bool>.Some(false)}),
+                new Tuple<int, int, FSharpOption<bool>[]>(
+                    3,0,
+                    new [] { FSharpOption<bool>.Some(false), FSharpOption<bool>.Some(true), FSharpOption<bool>.Some(false), FSharpOption<bool>.Some(true)}),
+                new Tuple<int, int, FSharpOption<bool>[]>(
+                    1,0,
+                    new [] { FSharpOption<bool>.Some(false), FSharpOption<bool>.Some(true), FSharpOption<bool>.Some(false), FSharpOption<bool>.Some(true)})
+            };
+            //TruthTableSimplifier.sliceRow(0,1, test)
+            foreach (var row in tests)
+            {
+                var firstValue = row.Item3[row.Item1];
+                var secondValue = row.Item3[row.Item2];
+                // Slice value out
+                var slicedresult = TruthTableSimplifier.sliceRow(row.Item1, row.Item2, row.Item3);
+                // Put value back in
+                var result = TruthTableSimplifier.insertInRow(row.Item1, firstValue, row.Item2, secondValue, slicedresult);
+                Assert.IsTrue(Enumerable.SequenceEqual(row.Item3, result), 
+                    $"Failed at {row.Item1}, {row.Item2}, {TruthTableSimplifier.rowToString(row.Item3)} got {TruthTableSimplifier.rowToString(result)}");
+            }
+        }
     }
 }
--- Ale1.View/Ale1.Parser/TruthTableSimplifier.fs
+++ Ale1.View/Ale1.Parser/TruthTableSimplifier.fs
@@ -30,19 +30,59 @@ let sliceRow (a : int) (b : int) (row : Option<bool>[]) =
     |> List.where (fun x -> x <> a && x <> b)
     |> List.map (fun x -> row.[x])
     |> List.toArray
+
+let insertInRow (a : int) (aValue : bool option) (b : int) (bValue : bool option) (row : Option<bool>[]) =
+    let (low, high) = if a < b then (a,b) else (b,a)
+    [0..(row.Length + 1)]
+    |> List.map (fun x ->
+        match x with
+        | i when i < low -> row.[x]
+        | i when i = a -> aValue
+        | i when i < high -> row.[x - 1]
+        | i when i = high -> bValue
+        | _ -> row.[x - 2]
+        )
+    |> List.toArray
+
+let rowToString (row: Option<bool>[]) =
+    row
+    |> Array.map(fun x ->
+        match x with
+        | Some(true) -> "1"
+        | Some(false) -> "0"
+        | None -> "*")
+    |> String.concat ""
+
 let private simplify (a : int) (count : int) (rows : SimpleTruthTableRow list) =
-    let q = rows.Head.Variables
     let trueRows = 
         rows
         |> List.where (fun x -> x.Variables.[a] = Some(true))
 
-    let otherRows = 
+    let simplifiedRows = 
         [0..(count - 2)]
         |> List.map (fun x -> 
             if x >= a then (a, x + 1) else (a, x))
+        // Created pairs like (1,0)  (1,2) (1,3)...
         |> List.map (fun (x, y) -> 
             trueRows
-            |> List.map (fun z -> (sliceRow x y z.Variables, z.Result)))
+            |> List.map (fun z -> (sliceRow x y z.Variables, z.Variables, z.Result)) // Slice columns, collumn x is true, collumn y can be both
+            |> List.groupBy (fun (k, _, _) -> rowToString k) // group by the same row, except for collumn x and y
+            |> List.map (fun (_, rows) -> 
+                let simplifyable = 
+                    rows
+                    |> List.exists(fun (_slicedrow, _row, result) -> not result)
+                    |> not
+                if simplifyable then
+                    rows
+                    |> List.map (fun (_slicedrow, row, result) ->
+                        new SimpleTruthTableRow(Variables = row, Result = result))
+                else
+                    rows
+                    |> List.map (fun (_slicedrow, row, result) ->
+                        new SimpleTruthTableRow(Variables = row, Result = result))
+                )
+            )
+        //|> List.groupBy (fun (x, _, _) -> x)
         
     rows
 let rec private iterate (a : int) (count : int) (rows : SimpleTruthTableRow list) =
