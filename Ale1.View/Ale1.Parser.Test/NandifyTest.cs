19424ffe592a18f86d95d5dfc73ec88b4442a6b2
Coen <coenstange@me.com>
2017-09-27 12:32:04 +0200

    + add nandify

--- Ale1.View/Ale1.Parser.Test/NandifyTest.cs
+++ Ale1.View/Ale1.Parser.Test/NandifyTest.cs
@@ -12,11 +12,11 @@ namespace Ale1.Parser.Test
         {
             var tests = new Tuple<string, string>[]
             {
-                new Tuple<string, string>("&(a,b)", "&(a,b)"),
+                new Tuple<string, string>("&(a,b)", "%(%(a,b),%(a,b))"),
                 new Tuple<string, string>("%(a,b)", "%(a,b)"),
-                new Tuple<string, string>("|(a,b)", "%(~(a),~(b))"),
-                new Tuple<string, string>(">(a,b)", "%(~(~(a)),~(b))"),
-                new Tuple<string, string>("=(a,b)", "%(~(&(~(a),~(b))),~(&(a,b)))")
+                new Tuple<string, string>("|(a,b)", "%(%(a,a),%(b,b))"),
+                new Tuple<string, string>(">(a,b)", "%(%(%(a,a),%(a,a)),%(b,b))"),
+                new Tuple<string, string>("=(a,b)", "%(%(%(%(%(a,a),%(b,b)),%(%(a,a),%(b,b))),%(%(%(a,a),%(b,b)),%(%(a,a),%(b,b)))),%(%(%(a,b),%(a,b)),%(%(a,b),%(a,b))))")
             };
 
             foreach (var test in tests)
@@ -27,7 +27,7 @@ namespace Ale1.Parser.Test
                 Assert.AreEqual(test.Item2, result, "Not expected proposition");
                 var hashcode = BitarrayUtility.BitsToHex(TreeToTruthTable.CreateTruthTable(tree).Values);
                 var nandhashcode = BitarrayUtility.BitsToHex(TreeToTruthTable.CreateTruthTable(nandified).Values);
-                Assert.AreEqual(hashcode, nandhashcode, "Truthtables are not the same");
+                Assert.AreEqual(hashcode, nandhashcode, $"Truthtables are not the same for {test.Item1} and {test.Item2} ");
             }
         }
     }
--- Ale1.View/Ale1.Parser/NandifyTree.fs
+++ Ale1.View/Ale1.Parser/NandifyTree.fs
@@ -8,25 +8,29 @@ let rec private itterate (input: ITreeNode) =
     | :? TreeOperand as o->
         match o.NodeValue with // The NOT operand is the only without a right node
         | OperandValue.Not ->
-            upcast new TreeOperand(NodeValue = OperandValue.Not, Left = itterate o.Left)
-        | OperandValue.And | OperandValue.Nand ->
+            let leaf = itterate o.Left
+            upcast new TreeOperand(NodeValue = OperandValue.Nand, Left = leaf, Right = leaf)
+        | OperandValue.Nand ->
             let left = itterate o.Left
             let right = itterate o.Right
-            upcast new TreeOperand(NodeValue = o.NodeValue, Left = left, Right = right)
+            upcast new TreeOperand(NodeValue = OperandValue.Nand, Left = left, Right = right)
+        | OperandValue.And ->
+            let prop = new TreeOperand(NodeValue = OperandValue.Nand, Left = itterate o.Left, Right = itterate o.Right)
+            itterate (new TreeOperand(NodeValue = OperandValue.Not, Left = prop))
         | OperandValue.Or ->
-            let left = new TreeOperand(NodeValue = OperandValue.Not, Left = itterate o.Left)
-            let right = new TreeOperand(NodeValue = OperandValue.Not, Left = itterate o.Right)
+            let left = itterate (new TreeOperand(NodeValue = OperandValue.Not, Left = itterate o.Left))
+            let right = itterate (new TreeOperand(NodeValue = OperandValue.Not, Left = itterate o.Right))
             upcast new TreeOperand(NodeValue = OperandValue.Nand, Left = left, Right = right)
         | OperandValue.Implication ->
-            let left = new TreeOperand(NodeValue = OperandValue.Not, Left = itterate o.Left)
+            let left = itterate (new TreeOperand(NodeValue = OperandValue.Not, Left = itterate o.Left))
             itterate (new TreeOperand(NodeValue = OperandValue.Or, Left = left, Right = itterate o.Right))
         | OperandValue.BiImplication ->
             let leftIt = itterate o.Left
             let rightIt = itterate o.Right
-            let left = new TreeOperand(NodeValue = OperandValue.And, 
-                Left = new TreeOperand(NodeValue = OperandValue.Not, Left = leftIt), 
-                Right = new TreeOperand(NodeValue = OperandValue.Not, Left = rightIt))
-            let right = new TreeOperand(NodeValue = OperandValue.And, Left = leftIt, Right = rightIt)
+            let left = itterate(new TreeOperand(NodeValue = OperandValue.And, 
+                Left = itterate (new TreeOperand(NodeValue = OperandValue.Not, Left = leftIt)), 
+                Right = itterate (new TreeOperand(NodeValue = OperandValue.Not, Left = rightIt))))
+            let right = itterate (new TreeOperand(NodeValue = OperandValue.And, Left = leftIt, Right = rightIt))
             itterate (new TreeOperand(NodeValue = OperandValue.Or, Left = left, Right = right))
         
 
